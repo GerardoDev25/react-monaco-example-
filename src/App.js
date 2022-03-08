aef1560428f4deb95a12ccad69785e60090c19f1
Gerardo Miranda <gerardodev25@gmail.com>
2022-03-07 22:32:28 -0400

    example finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Editor from '@monaco-editor/react';
+import { useState, useRef } from 'react';
 
 const App = () => {
+  const [content, setContent] = useState('dsdsd');
+  const editorRef = useRef(null);
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+    console.log({ editor, monaco });
+  };
+
+  const handleSave = () => {
+    console.log(editorRef.current.getValue());
+  };
+
   return (
     <div>
-      <Editor height="90vh" theme="vs-dark" defaultLanguage="typescript" />
+      <button onClick={handleSave}>Save</button>
+      <Editor
+        //
+        height="90vh"
+        theme="vs-dark"
+        value={content}
+        defaultLanguage="markdown"
+        // onChange={handleChange}
+        onChange={(value) => setContent(value)}
+        onMount={handleEditorDidMount}
+      />
     </div>
   );
 };
